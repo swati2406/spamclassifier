d611a885c8c3a38b198592da6d2abb295acfe4f6
swati2406 <swatimunda24@gmail.com>
2022-10-10 15:22:44 +0530

    Added the NLP model files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # spamclassifier
+
+### Software and Tools requirements
+1. [Github Account](https://github.com)
+2. [HerokuAccount](https://heroku.com)
+3. [VSCodeIDE](https://code.visualstudio.com)
+4. [GitCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Command-Line)
+
+Create a new environment for project
+```
+conda create -p venv python==3.7 -y
+```
+Activate env
+```
+conda activate --venv_filepath
+
+```
+connect git cli with ur git hub account
+git config --global user.name
+git config --global user.email
---
+++ spamClassifier.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 07:49:11 2022
+
+@author: swati
+
+Task- Spam Classifier
+
+Process-
+1. read the file
+2. Data preprocessing
+3. Train model
+4. Test model
+5. Pickle model file for deployment
+"""
+
+#reading file
+import pandas as pd
+messages = pd.read_csv('/home/swati/nltk_data/smsspamcollection/SMSSpamCollection',sep='\t',names=['label','messages'])
+
+#data preprocessing
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+corpus = []
+lemmatizer = WordNetLemmatizer()
+for i in range(len(messages)):
+    lines = re.sub('[^A-Za-z]',' ',messages.iloc[i,1])
+    lines = lines.lower()
+    words = lines.split(' ')
+    words =[lemmatizer.lemmatize(word) for word in words if word not in set(stopwords.words('english'))]
+    words = ' '.join(words)
+    corpus.append(words)
+    
+tfidf= TfidfVectorizer(max_features=5000)
+X= tfidf.fit_transform(corpus).toarray()    
+
+y=pd.get_dummies(messages['label'],drop_first=True)
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.3,random_state=42)
+
+from sklearn.naive_bayes import MultinomialNB
+NB_model = MultinomialNB().fit(X_train, y_train)
+y_prediction = NB_model.predict(X_test)
+
+from sklearn.metrics import classification_report,confusion_matrix
+print(classification_report(y_test, y_prediction))
+print(confusion_matrix(y_test, y_prediction))
+
+import pickle
+pickle.dump(NB_model,open('SpamClassifier.pickle','wb'))
